87a1d9b28b336a9f7343c71c5b14780fe0f9ee73
Marc Frances <marc.frances.t@gmail.com>
2023-12-06 20:58:32 +0100

    Initial setup

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -1,5 +1,6 @@
 import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
+import styles from '@/app/ui/invoices/status.module.css';
 
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
@@ -7,8 +8,8 @@ export default function InvoiceStatus({ status }: { status: string }) {
       className={clsx(
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
         {
-          'bg-gray-100 text-gray-500': status === 'pending',
-          'bg-green-500 text-white': status === 'paid',
+          [styles.pending]: status === 'pending',
+          [styles.paid]: status === 'paid',
         },
       )}
     >
