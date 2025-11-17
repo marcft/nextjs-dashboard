9c923114ed21e41ffe36c2cd71594e37171597f2
Marc Frances <marc.frances.t@gmail.com>
2025-11-17 12:53:39 +0100

    Implement customers screen

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,21 @@
 import { Metadata } from 'next';
+import Table from '@/app/ui/customers/table';
+import { FormattedCustomersTable } from '@/app/lib/definitions';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function CustomersPage() {
-  return <p>Customers Page</p>;
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return <Table customers={customers} />;
 }
